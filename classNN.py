be0d27cc4d8652ec67a7060b8da93d8a2dd15984
johnLIC <yesthatjohn@hotmail.com>
2024-12-17 22:45:41 -0500

    Update classNN.py

--- classNN.py
+++ classNN.py
@@ -1,8 +1,6 @@
 import torch
 from torch import nn
-#from torch.utils.data import DataLoader
-#from torchvision import datasets
-#from torchvision.transforms import ToTensor
+
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
